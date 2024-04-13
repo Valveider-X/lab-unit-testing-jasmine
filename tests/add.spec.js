a8a4f6f6dfca98a5916a5a052a38a0831c13a86a
Xavi <valcreativo@gmail.com>
2024-04-13 12:36:56 +0200

    Solved lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,18 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add(1, "2")).toEqual(undefined);
+        expect(add("3", 4)).toEqual(undefined);
+        expect(add("100", "47")).toEqual(undefined);
+      });
     });
   });
-  
+  /* Test
+  1- 4 pruebas
+  2- describe: es lo que tienes que hacer en este caso "Add"/"Añadir"
+     it: Son unas condiciones que el test debe pasar para ver que es correcto. 
+  3- las descripciones se hacen en comillas como strings.
+  4- los expect son lo que se espera que tu código haga. por ejemplo la suma de 100 y 47 debe dar 147
+
+  */
